2d2bb63be7451c9f150be98602e4da82a00e4fbc
Pham Quan <c2202lm.pmquan@aptech.vn>
2023-10-22 18:39:50 +0700

    abc

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -6,8 +6,15 @@ import Container from "react-bootstrap/esm/Container";
 import Card from "react-bootstrap/Card";
 import "./assets/CSS/productDetail.css";
 import "./assets/CSS/button.css";
+import { useState } from "react";
 
-function Detail(){
+function Detail(props){
+    const [detail,setDetail] = useState({
+        title:"ST15K",
+        reducer: "Free Call",
+        speed:"12GB/s",
+        description:"Dùng rất là sướng, nhanh mạnh khỏe, chơi game mượt, ưu đãi tốt, được nhiều idol recomend"
+    });
     return(
         <div className="bg-detail">
           <Header/>
@@ -25,19 +32,19 @@ function Detail(){
             </Col >
             <Col xs={12} sm={6} md={8} lg={8} className="p-3 my-3 img-block postition-relative">
                 
-                <p className=" uppercase text-custom-title"><a href="" className="text-decoration-none text-custom-title">Card Title</a></p>
+                <p className=" uppercase text-custom-title"><a href="" className="text-decoration-none text-custom-title">{detail.title}</a></p>
                 <p className="mt-3 text-success price">120000đ<span> /tháng</span></p>
-                <span className="text-custom">Ưu đãi: </span> <span className="text-danger text-custom-title"> Free call</span>
+                <span className="text-custom">Ưu đãi: </span> <span className="text-danger text-custom-title"> {detail.reducer}</span>
                 <div className = "d-flex">
                 <Col md={6} lg={6} className="text-custom my-3 ">
 <span className="text-custom-title">Băng thông :</span><span className="text-custom-title">Băng thông quốc tế</span>
                 </Col>
                 <Col md={6} lg={6} className="text-custom my-3 ">
-                <span className="text-custom-title">Tốc độ đường truyền :</span><span className="text-custom-title">12GB/s</span>
+                <span className="text-custom-title">Tốc độ đường truyền :</span><span className="text-custom-title">{detail.speed}</span>
                 </Col>
                
                 </div>
-                <p className="text-custom"><span className="text-custom-title">Mô tả :</span>Dùng rất là sướng, nhanh mạnh khỏe, chơi game mượt, ưu đãi tốt, được nhiều idol recomend</p>
+                <p className="text-custom"><span className="text-custom-title">Mô tả :</span>{detail.description}</p>
                  <div className="my-3"><a href="/"><button className="bn632-hover bn28">Đăng ký</button></a></div>
                 <div className="detail1"></div>
            
--- src/component/RegisterForm.jsx
+++ src/component/RegisterForm.jsx
@@ -1,8 +1,15 @@
 
+import { useEffect, useState } from "react";
 import { Row,Col } from "react-bootstrap"
 import { Form } from "react-bootstrap"
 import Container from "react-bootstrap/Container";
 export default function RegisterForm(){
+  const city_data = [
+    {id:1, name:"Hà Nội",data_district:[{d_id:1,d_name:"dt1"},{d_id:2,d_name:"dt2"}]},
+    {id:2, name:"Hải Phòng",data_district:[{d_id:3,d_name:"dt3"},{d_id:4,d_name:"dt4"}]}
+  ]
+  const [city,setCity] = useState({});
+  const [district,setDistrict] = useState({});
 return(
   <>
   <Container>
@@ -42,11 +49,15 @@ return(
 
                   </Row>
                   <Row>
-                    <Form.Select aria-label="Default select example" className="w-25 mb-4">
-                      <option>Thành phố</option>
-                      <option value="1">One</option>
-                      <option value="2">Two</option>
-                      <option value="3">Three</option>
+                    <Form.Select aria-label="Default select example" className="w-25 mb-4" onChange={(e)=>{
+                        setCity(e.target.value)
+                    }}>
+                      <option>--Thành phố--</option>
+                      
+                      {city_data.map((item) => (
+		
+                      <option value={item.id}>{item.name}</option>
+                        ))}
                     </Form.Select>
                     <Form.Select aria-label="Default select example" className="w-25 mb-4 mx-2">
                       <option>Quận</option>
